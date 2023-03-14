2d36dc32fa9bec735003b4578de3f878bdc2842c
Fanny Andréo <75836038+fannya76@users.noreply.github.com>
2023-03-14 08:27:55 +0100

    Update flip.js

--- flip.js
+++ flip.js
@@ -7,19 +7,16 @@ let btnVerso = document.getElementById('btn-verso')
 
 btnRecto.addEventListener('click', function() {
     card.style.transform = "rotateY(180deg)"
-    // document.getElementById('card').style.transform = "rotateY(180deg)"
 })
 btnVerso.addEventListener('click', function() {
     card.style.transform = ""
-    // document.getElementById('card').style.transform = ""
 })
 
 // Mail select et copie on click
 let mail = document.getElementById('mail')
 
-mail.addEventListener('click', function(event) {
-      let copyText = mail.textContent;
-      navigator.clipboard.writeText(copyText);
-      alert("Adresse Copiée : " + copyText);   
-      console.log(navigator.clipboard.readText()); 
+mail.addEventListener('click', function() {
+      let mailText = mail.innerText;
+      navigator.clipboard.writeText(mailText);
+      alert("Adresse Copiée : " + mailText);   
   });
